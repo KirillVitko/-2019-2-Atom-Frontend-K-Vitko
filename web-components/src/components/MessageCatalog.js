6c3b0d691569c9e42baccbe3d0067ea82f375e3a
Kirill <vitko.1998@mail.ru>
2019-11-18 15:40:16 +0200

    HW3+HW4

--- web-components/src/components/MessageCatalog.js
+++ web-components/src/components/MessageCatalog.js
@@ -21,6 +21,8 @@ template.innerHTML = `
 class MessageCatalog extends HTMLElement {
     constructor (){
         super()
+        /* eslint no-underscore-dangle: ["error", { "allow": ["_shadowRoot"] }] */
+        /* eslint camelcase: 0 */
         this._shadowRoot = this.attachShadow({ mode: 'open' })
         this._shadowRoot.appendChild(template.content.cloneNode(true))
         this.$container = this.shadowRoot.querySelector('.message')
@@ -32,15 +34,23 @@ class MessageCatalog extends HTMLElement {
     }
 
     attributeChangedCallback(name, old_value, new_value) {
-          const message = {}
-          const time = new Date()
-          const user_name = "kirill"
-          message.text = new_value
-          message.time = `${time.getHours()}:${time.getMinutes()}`
-          message.name = user_name
-          this.message_list.push(message)
-          const json = JSON.stringify(this.message_list)
-          localStorage.setItem('message-container', json)
+        const message = {}
+        const time = new Date()
+        const user_name = "kirill"
+        message.text = new_value
+        message.time = `${time.getHours()}:${('0'+time.getMinutes()).slice(-2)}`
+        message.name = user_name
+        this.message_list.push(message)
+        const json = JSON.stringify(this.message_list)
+        localStorage.setItem('message-container', json)
+
+        const newmessage = document.createElement('message-value')
+        newmessage.setAttribute('text', message.text)
+        newmessage.setAttribute('time', message.time)
+        newmessage.setAttribute('name', message.name)
+        this.$container.scrollBottom = this.$container.scrollHeight
+        this.$container.prepend(newmessage)
+        newmessage.scrollIntoView()
     }
 
     connectedCallback() {
--- web-components/src/components/MessageValue.js
+++ web-components/src/components/MessageValue.js
@@ -17,12 +17,37 @@ template.innerHTML = `
         }
 
         .message {
+            animation: fadeIn 1s;
             max-width: 100%;
             margin: 10px;
             background-color: #ADFF2F;
             padding: 7px;
             border-radius:10px;
             border: 2px;
+            transition-property: background;
+            transition-duration: .5s;
+        }
+
+        @keyframes fadeIn {
+          0% {
+              transform: scale(0);
+              opacity: 0.0;
+          }
+          60% {
+              transform: scale(1.1);
+          }
+          80% {
+              transform: scale(0.9);
+              opacity: 1;
+          }
+          100% {
+              transform: scale(1);
+              opacity: 1;
+          }
+      }
+
+        .message:active {
+          background: #00BFFF;
         }
     </style>
 
@@ -37,6 +62,10 @@ template.innerHTML = `
 class MessageValue extends HTMLElement {
     constructor (){
         super()
+        /* eslint no-underscore-dangle: ["error", { "allow": ["_shadowRoot"] }] */
+        /* eslint camelcase: 0 */
+        /* eslint no-multi-assign: 0 */
+        /* eslint prefer-destructuring: 0 */
         this._shadowRoot = this.attachShadow({ mode: 'open' })
         this._shadowRoot.appendChild(template.content.cloneNode(true))
         this.$text = this.shadowRoot.querySelector('.text')
@@ -63,17 +92,8 @@ class MessageValue extends HTMLElement {
     }
 
     attributeChangedCallback(name, old_value, new_value) {
-        if (name == 'text') {
-          this.message.push(new_value)
-        }
-        else if (name == 'time') {
-          this.message.push(new_value)
-        } else {
-          this.message.push(new_value)
-        }
+        this.message.push(new_value)
     }
-
-
 }
 
 customElements.define('message-value', MessageValue);
